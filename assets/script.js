397002c6e1f40c2f6faaacf9b9886bdcf3cc83c1
Ayako Woollan <ayako.woollan@gmail.com>
2022-01-04 15:53:06 +1100

    updating javascript

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,10 @@ const clearBtn = document.getElementById("clearBtn");
 const searchInput = document.getElementById("searchInput");
 const searchedCities = document.getElementById("searchedCities");
 
+var userInputArr = JSON.parse(localStorage.getItem("savedCities")) || [];
 
 
-var searchUrl = "https://openweathermap.org/data/2.5/weather?q=";
-const apiKey = "97f07c142393e325b412688726de1147";
+
 
 
 searchBtn.addEventListener("click", function (event) {
@@ -27,25 +27,38 @@ clearBtn.addEventListener("click", function () {
     localStorage.removeItem("savedCities");
     searchedCities.innerText = ' ';
 
-})
+});
+
 
 function saveRecentSearches() {
-    var userInputArr = JSON.parse(localStorage.getItem("savedCities")) || [];
     var userSearchInput = searchInput.value;
     userInputArr.push(userSearchInput);
     localStorage.setItem("savedCities", JSON.stringify(userInputArr));
-
     userInputArr.reverse();
+    displayRecentSearches();
+}
+
+function displayRecentSearches() {
 
     userInputArr.forEach(function (item, index) {
         if (index < 5) {
             const cityBtn = document.createElement("button");
             cityBtn.textContent = item;
-            cityBtn.setAttribute("text-transform", "capitalize");
+            cityBtn.style.textTransform = "capitalize";
             cityBtn.setAttribute("class", "btn");
             searchedCities.appendChild(cityBtn);
 
         }
     })
-
 }
+
+var searchUrl = `https://openweathermap.org/data/2.5/weather?q=${location}97f07c142393e325b412688726de1147`;
+
+
+function getWeather(searchUrl) {
+    fetch(searchUrl)
+        .then(function (response) {
+
+        }.catch(function (error) {
+            alert("Please enter a city name again.");
+        }) 
